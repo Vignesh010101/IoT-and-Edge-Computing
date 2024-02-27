17188c3f6024e3903306196ca6064de875fcb69c
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-02-27 22:20:23 +0530

    Add files via upload

---
+++ tcp_file_clnt.py
@@ -0,0 +1,26 @@
+#Client
+
+import socket
+
+srvr_host = '10.114.4.51'
+srvr_port = 35791
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((srvr_host, srvr_port))
+
+# Send the file name
+file_name = 'chatgpt.txt'  # Change this to the name of the file you want to send
+client_socket.sendall(file_name.encode())
+
+# Open the file in binary read mode
+with open(file_name, 'rb') as file:
+    # Read and send data in chunks
+    data = file.read(1024)
+    while data:
+        client_socket.sendall(data)
+        data = file.read(1024)
+
+print("[*] File sent successfully.")
+
+# Close the connection
+client_socket.close()
